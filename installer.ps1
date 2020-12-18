ca2ab0a14839ba4eeeb76b95a218172b366150fa
paukkle <paukkle@gmail.com>
2020-12-18 20:24:05 +0200

    turhat pois

--- installer.ps1
+++
@@ -1,5 +0,0 @@
-curl https://github.com/paukkle/Hirviopeli/archive/main.zip -O .\hirviopeli.zip
-Expand-Archive -LiteralPath .\hirviopeli.zip -DestinationPath .\ -Force
-Get-ChildItem -Path .\Hirviopeli-main | Copy-Item -Destination .\ -Force -Recurse
-Remove-Item .\Hirviopeli-main -Recurse
-Remove-Item .\*.* -Exclude *.exe
